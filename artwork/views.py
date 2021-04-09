4fbfb8eeb7edd92818bb8010c2d8bb591e1a295e
92an <micke92an@outlook.com>
2021-04-09 20:16:19 +0000

    category selection implemented

--- artwork/views.py
+++ artwork/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Artwork
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Artwork, Medium
 
 # Create your views here.
 
@@ -8,9 +10,28 @@ def artwork(request):
     """A view of the artwork for sale, as well as sorting and filtering"""
 
     artworks = Artwork.objects.all()
+    query = None
+    mediums = None
+
+    if request.GET:
+        if "medium" in request.GET:
+            mediums = request.GET["medium"].split(",")
+            artworks = artworks.filter(medium__name__in=mediums)
+            mediums = Medium.objects.filter(name__in=mediums)
+
+        if "q" in request.GET:
+            query = request.GET["q"]
+            if not query:
+                messages.error(request, "you need to enter something into the search bar")
+                return redirect(reverse("artwork"))
+
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            artworks = artworks.filter(queries)
 
     context = {
         "artworks": artworks,
+        "search_term": query,
+        "current_medium": mediums,
     }
 
     return render(request, "artwork/artwork.html", context)
