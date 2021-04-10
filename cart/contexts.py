9eeeb4516ff248e54453eaf0d2208c9afb09e9aa
92an <micke92an@outlook.com>
2021-04-10 17:24:05 +0000

    adding product to cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,15 +1,31 @@
-
+from django.conf import settings
+from django.shortcuts import get_object_or_404 
+from artwork.models import Artwork
 
 def cart_content(request):
 
-    bag_items = []
+    cart_items = []
     total = 0
-    product_count = 0
+    artwork_count = 0
+    cart = request.session.get("cart", {})
+
+    for item_id, quantity in cart.items():
+        artwork = get_object_or_404(Artwork, pk=item_id)
+        total += quantity * artwork.price
+        artwork_count += quantity
+        cart_items.append({
+            "item_id": item_id,
+            "quantity": quantity,
+            "artwork": artwork,
+        })
+
+    grand_total = total
 
     context = {
-        "bag_items": bag_items,
+        "cart_items": cart_items,
         "total": total,
-        "product_count": product_count,
+        "artwork_count": artwork_count,
+        "grand_total": grand_total,
     }
 
     return context
