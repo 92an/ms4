6fd4b439f6b744098989ca351497cdf74ac583c8
92an <micke92an@outlook.com>
2021-04-10 19:24:29 +0000

    fixed cart display and fixed code for discount price

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404 
 from artwork.models import Artwork
@@ -6,27 +7,33 @@ def cart_content(request):
 
     cart_items = []
     total = 0
+    total_discount = 0
     artwork_count = 0
     cart = request.session.get("cart", {})
 
     for item_id, quantity in cart.items():
         artwork = get_object_or_404(Artwork, pk=item_id)
-        total += quantity * artwork.price
+        if artwork.price_discounted:
+            total_discount += quantity * artwork.price_discounted
+        else:
+            total += quantity * artwork.price
         artwork_count += quantity
         cart_items.append({
             "item_id": item_id,
             "quantity": quantity,
             "artwork": artwork,
+            "total": total,
+            "total_discount": total_discount,
         })
 
-    grand_total = total
+    grand_total = total + total_discount
 
     context = {
         "cart_items": cart_items,
         "total": total,
+        "total_discount": total_discount,
         "artwork_count": artwork_count,
         "grand_total": grand_total,
     }
 
     return context
-    
