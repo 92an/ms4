2f1e3061b76b0677af24829fb2fb7a5d2e83c59e
92an <micke92an@outlook.com>
2021-06-05 00:40:28 +0000

    toast messages implemented

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from artwork.models import Artwork
 
@@ -11,13 +11,14 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
 
-    artwork = Artwork.objects.get(pk=item_id)
+    artwork = get_object_or_404(Artwork, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request,  f"Updated {artwork.title} quantity to {cart[item_id]}")
     else:
         cart[item_id] = quantity
         messages.success(request,  f"Added {artwork.title} to your cart")
@@ -28,13 +29,16 @@ def add_to_cart(request, item_id):
 
 def adjust_cart(request, item_id):
 
+    artwork = get_object_or_404(Artwork, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request,  f"Updated {artwork.title} quantity to {cart[item_id]}")
     else:
         cart.pop(item_id)
+        messages.success(request,  f"Removed {artwork.title} from your cart")
 
     request.session['cart'] = cart
     return redirect(reverse("view_cart"))
@@ -43,11 +47,14 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
 
     try:
+        artwork = get_object_or_404(Artwork, pk=item_id)
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(request,  f"Removed {artwork.title} from your cart")
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request,  f"Error removing item: {e}")
         return HttpResponse(status=500)
