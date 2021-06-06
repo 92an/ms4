82e25121d25a961393a7ed9a24798bf271219b43
92an <micke92an@outlook.com>
2021-06-06 17:18:28 +0000

    strip elemets added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IzLngLcyLgSgJ3NvVWvR6YfXLg4RyEpzDnJc5CBRjBJPaEe7CAWLmwoVjvTnARyyZodhBhyKSSEcl3eP8Ye9Oji00Mnkcq3nE',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
