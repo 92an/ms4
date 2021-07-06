e262f007a5bf142050bbabddfe63c70fda649ad7
92an <micke92an@outlook.com>
2021-07-06 21:23:22 +0000

    webhook handler implementation expanded with events

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,6 +7,25 @@ class StripeWH_Handler:
         self.request = request
 
     def handle_event(self, event):
+        """
+        Handle generic/unknown/unexpected event
+        """
         return HttpResponse(
-            content = f"Webhook Received: {event["type"]}", 
-            status = 200)    
+            content = f"Unhandled Webhook Received: {event['type']}", 
+            status = 200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle payment intent succeded event
+        """
+        return HttpResponse(
+            content = f"Webhook Received: {event['type']}", 
+            status = 200)   
+
+    def handle_payment_intent_failed(self, event):
+        """
+        Handle payment intent failed event
+        """
+        return HttpResponse(
+            content = f"Webhook Received: {event['type']}", 
+            status = 200)  
