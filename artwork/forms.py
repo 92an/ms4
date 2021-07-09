a38be46eec190e66b74638453eb85cec722146a5
92an <micke92an@outlook.com>
2021-07-09 08:30:57 +0000

    admin artwork form

---
+++ artwork/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Artwork, Medium'
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        mediums = Medium.objects.all()
+        friendly_names = [(m.id, m.get_friedly_name()) for m in mediums]
+
+        self.fields["medium"].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "stripe-style-input"
